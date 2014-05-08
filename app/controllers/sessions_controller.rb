ff763e07f7bafb7e06ffdfb48dcda490650b6dcc
Ruthan <rcfreese@gmail.com>
2014-05-08 15:49:38 -0400

    Finish user actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password]) # is authenticate included free? 
 			sign_in user  # aha, a stub!!
-			redirect_to user
+			redirect_back_or user
 		else
 			flash.now[:error] = 'Invalid email/password combination' # could be more helpful ... 
 			render 'new'
